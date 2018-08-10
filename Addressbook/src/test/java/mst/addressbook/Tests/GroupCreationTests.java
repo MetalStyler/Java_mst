19192575bc80f60c8b6787c5cc866750bab154af
MetalStyler <metalstyler@gmail.com>
2018-08-10 14:54:38 +0300

    Group amount check added.

--- Addressbook/src/test/java/mst/addressbook/Tests/GroupCreationTests.java
+++ Addressbook/src/test/java/mst/addressbook/Tests/GroupCreationTests.java
@@ -1,17 +1,21 @@
 package mst.addressbook.Tests;
 
 import mst.addressbook.Model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
+        int before = app.getGroupHelper().getGroupCount();
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
         app.getGroupHelper().fillGroupForm(new GroupData("test_groupname", "test_groupheader", "test_groupfooter"));
         app.getGroupHelper().submitGroupGreation();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
